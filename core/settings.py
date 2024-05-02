030f962edd6ef2d4bf09eb938a7c218893dcdebc
SebastianMou <Sebastian_Mou@protonmail.com>
2024-05-01 22:54:04 -0400

    pyanywhere deplay 1

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#w6h08m+s3-nkc(p+wzv@vwz(dhv6xm-hk_w!l80t+p%j5rd4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # ALLOWED_HOSTS = [
 #     'localhost',
